85e38822bf3a10cf2eb35c3b8f9e5dbd5312e08a
Warita <warayasyp@gmail.com>
2025-01-15 12:41:06 +0100

    Estilo Web
    Arreglado el problema de el boton cookies

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.addEventListener("scroll", () => {
       headers.forEach((header) => (header.style.display = "none"));
       headers[index].style.display = "block";
     } else {
-      headers[index].style.display = "none"; // Oculta cuando no es la sección activa
+      headers[index].style.display = "none"; 
     }
   });
 });
@@ -27,21 +27,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Verificar si ya se mostró el aviso de cookies en esta sesión
   if (!sessionStorage.getItem("aviso-cookies")) {
-    avisoCookies.style.display = "block"; // Muestra el aviso de cookies
+    avisoCookies.style.display = "block"; //
   }
 
   // Aceptar cookies
   aceptarBoton.addEventListener("click", () => {
-    sessionStorage.setItem("aviso-cookies", "aceptado"); // Guardar aceptación en la sesión
-    avisoCookies.style.display = "none"; // Ocultar el aviso de cookies
-    // Aquí puedes agregar lógica adicional para habilitar cookies si fuera necesario
+    sessionStorage.setItem("aviso-cookies", "aceptado");
+    avisoCookies.style.display = "none"; 
   });
 
   // Rechazar cookies
   rechazarBoton.addEventListener("click", () => {
-    sessionStorage.setItem("aviso-cookies", "rechazado"); // Guardar rechazo en la sesión
-    avisoCookies.style.display = "none"; // Ocultar el aviso de cookies
-    // Aquí puedes agregar lógica adicional para deshabilitar cookies si fuera necesario
+    sessionStorage.setItem("aviso-cookies", "rechazado"); 
+    avisoCookies.style.display = "none"; 
+
   });
 });
 
